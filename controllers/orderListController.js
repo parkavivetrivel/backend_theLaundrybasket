c664f87d478b732ec6447a16385aed13440be82d
parkavivetrivel <bhargavivetrivel27@gmail.com>
2023-07-28 19:32:58 +0530

    okey4

--- controllers/orderListController.js
+++ controllers/orderListController.js
@@ -1,13 +1,22 @@
 const asyncHandler = require("express-async-handler");
-const { MongoClient } = require('mongodb');
+const { MongoClient,ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://parkavivetrivel:bhargavi@cluster0.d7l6lca.mongodb.net/?authMechanism=DEFAULT";
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const postorderList = asyncHandler(async (req, res) => {
+  try {
   console.log("Body", req.body);
+
   await client.connect();
   const db = client.db('TheLaundryBasket');
-  db.collection('orderList').insertOne({
+  const collection = db.collection('orderList');
+  const third = await collection.insertOne({
             ServiceType:req.body.ServiceType ,
             NoOfShirts:req.body.NoOfShirts,
             NoOfPants:req.body.NoOfPants,
@@ -17,14 +26,18 @@ const postorderList = asyncHandler(async (req, res) => {
             NoOfSaree:req.body.NoOfSaree,
             NoOfShaul:req.body.NoOfShaul,
             totalitems:req.body.totalitems,
-            totalamount:req.body.totalamount 
-    
-
+            totalamount:req.body.totalamount,
   });
+  console.log(third);
   res.status(201).send(true);
+}
+
+finally {
+  // Close the database connection when finished or an error occurs
+  await client.close();
+}
+
 });
-
-
 module.exports = {
     postorderList
 }
